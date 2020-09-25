eaa62469727120e042ddee6fb76aad5e87677b71
f4b1an <fabian.stork@student.hu-berlin.de>
2020-09-25 14:26:25 +0200

    First commit

---
+++ prep_script.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+knwon = pd.read_csv("C:/Users/Fabian/Desktop/Studium/2.) HU Berlin/5.) WS_2021/Dummy Project/BADS_WS1819_known.csv")
+unknown = pd.read_csv("C:/Users/Fabian/Desktop/Studium/2.) HU Berlin/5.) WS_2021/Dummy Project/BADS_WS1819_unknown.csv")
+
+# to be continued
